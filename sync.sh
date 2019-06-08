ac3769742139fa7a7d355d0388f3519d4526afba
Harry Ying <lexugeyky@outlook.com>
2019-06-08 21:07:46 +0800

    Add i3wm config

--- sync.sh
+++ sync.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 
 # sync Emacs configs
-echo "Syncing Emacs configs..."
+echo -n "Syncing Emacs configs..."
 cp ~/.emacs.d/init.el $(pwd)/emacs
 cp -r ~/.emacs.d/lisp $(pwd)/emacs
+echo "Done"
+
+# sync i3wm config
+echo -n "Syncing i3wm config..."
+cp ~/.config/i3/config  $(pwd)
 echo "Done"
