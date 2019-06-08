faabdb8ad047a99d4e6bf02602b326a09882d9c6
Harry Ying <lexugeyky@outlook.com>
2019-06-08 22:14:24 +0800

    Split custom into separated el

--- emacs/init.el
+++ emacs/init.el
@@ -23,17 +23,6 @@
 (require 'init-ui)
 
 ;; custom
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (flycheck lsp-ui rust-mode monokai-theme company))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'init-custom)
 
 ;;; init.el ends here
