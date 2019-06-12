698bfaa6a4ea1d0b212f671dc9792563840dbf8a
Harry Ying <lexugeyky@outlook.com>
2019-06-12 20:33:15 +0800

    Update the settings about auto-save feature

--- emacs/lisp/init-better-default.el
+++ emacs/lisp/init-better-default.el
@@ -7,10 +7,15 @@
 ;; highlights current line
 (global-hl-line-mode 1)
 
-;; turn off the auto-backup and enhance the auto-save feature
-(setq make-backup-files nil)
-(setq backup-inhibited t)  ;; turn off backup
-(auto-save-visited-mode)
+;; enhance the backup and auto-save features
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup/"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
+(auto-save-visited-mode 1)
 
 (provide 'init-better-default)
 
